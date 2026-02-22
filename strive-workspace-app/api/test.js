b1e0a23863851aedebde29f5f29940480effe1a7
zunnoonwaheed <zunnoonwaheed@gmail.com>
2026-02-23 01:07:39 +0500

    Test CommonJS format for serverless function

--- strive-workspace-app/api/test.js
+++ strive-workspace-app/api/test.js
@@ -1,3 +1,3 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.status(200).json({ message: 'Test function works!', method: req.method });
-}
+};
